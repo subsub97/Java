ef98988e4482db3bde8988c7185185201c8b79d5
HO <godqhr721@gmail.com>
2022-07-08 16:58:13 +0900

    First - java

---
+++ Tutorial2/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	
+	final static double PI = 3.141592;
+	
+	final static int INT_MAX = 2147483647;
+
+	public static void main(String[] args) {
+		
+		int a =1;
+		int b =2;
+		System.out.println("a+b =" + (a+b));
+		System.out.println("a+b =" + (a-b));
+		System.out.println("a+b =" + (a*b));
+		System.out.println("a+b =" + (a/b));
+		System.out.println("a+b =" + (a%b));
+	}
+
+}
