6b4560bb7de8143262976e39bc86ed1379ab15d7
HO <godqhr721@gmail.com>
2022-07-09 15:18:02 +0900

    used double for-loop

---
+++ Tutorial6/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+	
+	final static int N = 15;
+
+	public static void main(String[] args) {
+		for(int i = 0; i <= N; i++)
+		{
+			for(int j=-N; j <= N;j++) {
+				if(i*i +j*j <=N*N) {
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
