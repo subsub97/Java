69813898953d087cfe1484fc91e4f4d85abbe555
HO <godqhr721@gmail.com>
2022-07-24 16:35:44 +0900

    동빈나 1~8강

---
+++ First_Exam/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String name ="김득호";
+		System.out.println(name);
+		
+		System.out.println(10 + 10);
+		System.out.println(30*30);
+		System.out.println(20-5);
+		System.out.println(40/3);
+		System.out.println(395%18);
+		
+		for(int i=0;i<=10;i++) {
+			for(int j=0 ; j <=10; j++) {
+			System.out.print("*");
+		}
+			System.out.println();
+	}}}
+
